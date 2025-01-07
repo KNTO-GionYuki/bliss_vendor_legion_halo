0fc5f05615deae7a9ba0e33524a98d82521a866a
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-07 20:39:19 +0900

    まあそうですよね

--- halo-vendor.mk
+++ halo-vendor.mk
@@ -1,4 +1,2 @@
 PRODUCT_SOONG_NAMESPACES +=	\
 	vendor/legion/halo
-PRODUCT_COPY_FILES +=		\
-	vendor/lib64/vendor.qti.hardware.AGMIPC@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.AGMIPC@1.0-impl.so
