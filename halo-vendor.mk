9b80c1d47ed4e846f48246b031fa2876120d5984
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-07 20:34:38 +0900

    コピー先のパスエラー修復

--- halo-vendor.mk
+++ halo-vendor.mk
@@ -1,4 +1,4 @@
 PRODUCT_SOONG_NAMESPACES +=	\
 	vendor/legion/halo
 PRODUCT_COPY_FILES +=		\
-	vendor/lib64/vendor.qti.hardware.AGMIPC@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/vendor.qti.hardware.AGMIPC@1.0-impl.so
+	vendor/lib64/vendor.qti.hardware.AGMIPC@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.AGMIPC@1.0-impl.so
